07e5105aac3d5ddeadb918e4b698b0df82fa6099
pruk <prukcomic@gmail.com>
2019-06-11 14:09:25 +0700

    add mainfile

---
+++ steamtradeseasiermatch.user.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name         SteamtradesEasierMatch
+// @icon         https://image.flaticon.com/icons/svg/34/34202.svg
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Easy search for steamtrades!
+// @author       Puzzlovaski
+// @match        https://www.steamtrades.com/trades*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// ==/UserScript==
+
+// ----------------------------------------Setting here---------------------------------------------
+var autoMatch = false;      // If true, start matching after page loaded.
+// -------------------------------------------------------------------------------------------------
+
+// ========================================Coding section===========================================
+// box html
+const easyHtml = "<a id='easyOpen' href='javascript:void(0)' style='margin-bottom:5px; text-decoration:underline; color:rgb(120,169,71)'>Easy search</a>\
+<div class='search_trades' id='easyBox'>\
+<textarea id='haveList' type='text' placeholder='I have\n.\n.\n.' value></textarea>\
+<textarea id='wantList' type='text' placeholder='I want\n.\n.\n.' value></textarea>\
+<div class='btn_action red'  id='easySearch'><i class='fa fa-search'></i></div>\
+</div>"
+$(document).ready(function(){
+
+    // Add search box
+    $(".search_trades").before(easyHtml);
+
+    // Add div for output on each trade
+    $(".row_inner_wrap").not(".is_faded").parent().append("<div class='match'></div>");
+
+    // get list from GM
+    $("#haveList").val(GM_getValue("HL"));
+    $("#wantList").val(GM_getValue("WL"));
+
+    // setting button and hide box
+    $("#easyBox").hide();
+    $("#easySearch").click(easySearch);
+    $("#easyOpen").click(function(){
+        $("#easyBox").slideToggle();
+    });
+
+    if(autoMatch){
+        easySearch();
+    }
+});
+
+function easySearch(){
+
+    // do matching
+    resolve();
+
+    // store list to GM
+    GM_setValue("HL", $("#haveList").val());
+    GM_setValue("WL", $("#wantList").val());
+}
+
+function resolve(){
+
+    // get input and clean falsy values.
+    var iH = $("#haveList").val().replace(/^\s*$(?:\r\n?|\n)/gm, '').split("\n").filter(Boolean);
+    var iW = $("#wantList").val().replace(/^\s*$(?:\r\n?|\n)/gm, '').split("\n").filter(Boolean);
+
+    // resolving each trade
+    $(".row_inner_wrap").not(".is_faded").each(async function(){
+        var href = $(this).find(".column_flex > h3 a").attr('href');
+        var hL = [];
+        var wL = [];
+
+        // get each trade page
+        await $.get(href, function(res){
+            var uH = $(res).find(".have").text();
+            var uW = $(res).find(".want").text();
+
+            // matching
+            iH.forEach(function(e){
+                if (uW.toLowerCase().indexOf(e.toLowerCase()) != -1){
+                    hL.push(e);
+                }
+            });
+            iW.forEach(function(e){
+                if (uH.toLowerCase().indexOf(e.toLowerCase()) != -1){
+                    wL.push(e);
+                }
+            });
+        });
+
+        // post to each trade
+        $(this).siblings("div.match").html("<p><b>[H]:</b> " + hL.toString() + "</p><p><b>[W]:</b> " + wL.toString() +"</p>");
+    });
+}
