cbab73da4220bfe3b42765104357cc56a738bdb1
pruk <prukcomic@gmail.com>
2019-06-21 03:17:19 +0700

    add hilight trade that match both list

--- steamtradeseasiermatch.user.js
+++ steamtradeseasiermatch.user.js
@@ -91,5 +91,10 @@ function resolve(){
 
         // post to each trade
         $(this).siblings("div.match").html("<p><b>[H]:</b> " + hL.toString() + "</p><p><b>[W]:</b> " + wL.toString() +"</p>");
-    });
+
+		// change background of tag that match both lists to green
+		if(hL.length > 0 && wL.length >0){
+            $(this).parent().css("background-color", "#bfff00");
+        }
+	});
 }
